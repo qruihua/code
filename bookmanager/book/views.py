e7772b5a75004e6fb9ce580162c4bbca5e62f913
张三 <zhangsan@itcast.cn>
2020-08-14 16:08:59 +0800

    路由的基本匹配

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+"""
+视图函数
+
+Python函数 
+1. 要求函数的第一个参数是 请求的实例对象
+2. 要求函数有响应的返回
+"""
+from django.http import HttpRequest
+from django.http import HttpResponse
+
+def index(request):
+
+    return HttpResponse('ok')
+
--- bookmanager/bookmanager/urls.py
+++ bookmanager/bookmanager/urls.py
@@ -16,6 +16,27 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# 这个是固定写法.
+# urlpatterns = []
+"""
+当用户在浏览器中输入网址后, 
+用户的网址会顺次和我们的 urlpatterns 中的每一项进行比较
+如果一致,则进入到响应的视图中
+如果不一致,则继续往下比较.
+直到所有的每一项都比较完成.如果最终都没有一致的,则返回 404
+
+
+http://127.0.0.1:8000/admin/
+
+我们的路由: http://ip:port/path/?key=value&key=value
+其中 http://ip:port/  和 ?key=value&key=value 是不进行路由匹配的
+
+"""
+from book.views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # 参数1: 路由
+    # 参数2: 视图函数名
+    path('index/',index),
 ]

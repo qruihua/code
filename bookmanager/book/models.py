3157e5ef0f7f6400b18b6a23b2b869ef76e4c28f
张三 <zhangsan@itcast.cn>
2020-08-14 14:56:42 +0800

    定义模型

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+"""
+2个表
+
+书籍表             --->   类
+id	name
+1	西游记
+2	三国演义
+
+人物表             --->    类
+id	name	gender	book
+1	孙悟空	False	1
+2	白骨精	True	1
+3	曹操	    False	2
+4	貂蝉	    True	2
+
+class 模型类名(models.Model):
+    字段名=models.类型(选项)
+    
+    类型 和 选项明天会重点讲解 今天就是先了解一下
+"""
+class BookInfo(models.Model):
+    # django会自动为我们生成一个id的主键
+    #id
+
+    # CharField 对应数据库中的 varchar(M)
+    name=models.CharField(max_length=10)
+
+
+# 今天晚上把面向对象的内容 回顾一下
+class PeopleInfo(models.Model):
+    name=models.CharField(max_length=10)
+    gender=models.BooleanField(default=False)  #设置默认值
+    #外键
+    # 主表数据删除,on_delete=models.CASCADE 从表数据也跟着删除
+    book=models.ForeignKey(BookInfo,on_delete=models.CASCADE)
+
+
+
+
+
+
+

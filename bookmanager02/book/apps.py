d3520daad0b98d7d97e818eb759d305e1493bb8f
张三 <zhangsan@itcast.cn>
2020-08-15 10:17:10 +0800

    创建新项目

--- bookmanager02/book/apps.py
+++ bookmanager02/book/apps.py
@@ -3,4 +3,3 @@ from django.apps import AppConfig
 
 class BookConfig(AppConfig):
     name = 'book'
-    verbose_name='Admin里显示'
---
+++ bookmanager02/book/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from book.views import index
+urlpatterns = [
+    path('index/',index)
+]

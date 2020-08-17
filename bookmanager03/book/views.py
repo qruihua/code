662d6f2b38efeb0c0d7e8590aefbca76a81ec9c6
张三 <zhangsan@itcast.cn>
2020-08-17 09:47:23 +0800

    关联查询的过滤查询1

--- bookmanager03/book/views.py
+++ bookmanager03/book/views.py
@@ -22,3 +22,29 @@ person=PeopleInfo.objects.get(id=1)
 #person.book 的值 是这个人物 关联的模型 的实例对象数据
 person.book
 person.book.commentcount
+
+
+###############关联查询过滤查询############################
+
+# 查询图书，要求图书人物为"郭靖"
+
+
+# BookInfo.objects.filter(关联模型类名小写__字段__运算符=值)
+# __ 2个连续的下划线
+
+
+BookInfo.objects.filter(peopleinfo__name__exact='郭靖')
+#或者
+BookInfo.objects.filter(peopleinfo__name='郭靖')
+
+# 查询图书，要求图书中人物的描述包含"八"
+BookInfo.objects.filter(peopleinfo__description__contains='八')
+
+
+
+
+
+
+
+
+

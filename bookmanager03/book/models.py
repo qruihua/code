17fe4a62508c581ff8cdaf8fbe206f7af45459ec
张三 <zhangsan@itcast.cn>
2020-08-17 09:39:25 +0800

    管理查询

--- bookmanager03/book/models.py
+++ bookmanager03/book/models.py
@@ -12,7 +12,8 @@ class BookInfo(models.Model):
     readcount = models.IntegerField(default=0, verbose_name='阅读量')
     commentcount = models.IntegerField(default=0, verbose_name='评论量')
     is_delete = models.BooleanField(default=False, verbose_name='逻辑删除')
-
+    # 隐藏的
+    # peopleinfo_set = [PeopleInfo,PeopleInfo,PeopleInfo,...]
     class Meta:
         db_table = 'bookinfo'  # 指明数据库表名
         verbose_name = '图书'  # 在admin站点中显示的名称
@@ -30,8 +31,10 @@ class PeopleInfo(models.Model):
     name = models.CharField(max_length=20, verbose_name='名称')
     gender = models.SmallIntegerField(choices=GENDER_CHOICES, default=0, verbose_name='性别')
     description = models.CharField(max_length=200, null=True, verbose_name='描述信息')
-    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE, verbose_name='图书')  # 外键
     is_delete = models.BooleanField(default=False, verbose_name='逻辑删除')
+    # 当我们在这里 定义了外键之后,
+    # 系统会在 主表中 创建一个 隐藏的 关联模型类名小写_set
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE, verbose_name='图书')  # 外键
 
     class Meta:
         db_table = 'peopleinfo'
--- bookmanager03/book/views.py
+++ bookmanager03/book/views.py
@@ -5,3 +5,20 @@ from django.shortcuts import render
 def index(request):
 
     return HttpResponse('ok')
+
+
+###############关联查询#############################
+"""
+查询书籍为1的所有人物信息
+
+查询人物为1的书籍信息
+"""
+from book.models import BookInfo,PeopleInfo
+
+book=BookInfo.objects.get(id=1)
+book.peopleinfo_set.all()
+
+person=PeopleInfo.objects.get(id=1)
+#person.book 的值 是这个人物 关联的模型 的实例对象数据
+person.book
+person.book.commentcount

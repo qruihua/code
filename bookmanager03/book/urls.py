70a2cf76200afddc706db698791407f67d80af30
张三 <zhangsan@itcast.cn>
2020-08-18 09:40:01 +0800

    验证数据的基本使用

--- bookmanager03/book/urls.py
+++ bookmanager03/book/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from book.views import index,readbook,login,login_json,header,detail
 from book.views import jsonresponse,to_index
+from book import views
+
+from django.urls import converters
+# converters 转换器 -- 验证数据的
+
 urlpatterns = [
     path('index/',index),
     #http://127.0.0.1:8000/1/100/
@@ -8,13 +13,18 @@ urlpatterns = [
     # 分类id/书籍id/
     # <book_id> 我们可以理解为占位符,它就接收了这个位置的数据
     #
-    path('<cat_id>/<book_id>/',readbook),
-    # 模拟登录
-    path('login/',login),
-    path('login_json/',login_json),
-    path('header/',header),
-    path('detail/',detail),
-    path('jsonresponse/',jsonresponse),
-    path('to_index/',to_index),
+    # path('<cat_id>/<book_id>/',readbook),
+    # # 模拟登录
+    # path('login/',login),
+    # path('login_json/',login_json),
+    # path('header/',header),
+    # path('detail/',detail),
+    # path('jsonresponse/',jsonresponse),
+    # path('to_index/',to_index),
+
+    ################
+    # tieba_id 期望是整形!!!
+    # 怎么约束数据呢???
+    path('p/<int:tieba_id>/',views.baidu_tieba),
 
 ]
